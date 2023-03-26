9f5c5ba09995d30cb71596f1fe46327a8f205f65
Brandon Huang <brandon@kingsinfo.com.tw>
2023-03-26 21:46:11 +0800

    mantine ready

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        colorScheme: "light",
+      }}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
