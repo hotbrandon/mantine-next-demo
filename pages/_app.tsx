71ce8aa92db860183da15d944344ce0a92e3757b
Brandon Huang <brandon@kingsinfo.com.tw>
2023-03-26 23:53:43 +0800

    finished light/dark mode toggler

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { MantineProvider } from "@mantine/core";
+import {
+  ColorSchemeProvider,
+  MantineProvider,
+  ColorScheme,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
+import MasterLayout from "@/pages/layouts/MasterLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-      }}
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
     >
-      <Component {...pageProps} />
-    </MantineProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme,
+        }}
+      >
+        <MasterLayout>
+          <Component {...pageProps} />
+        </MasterLayout>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
---
+++ pages/demo/card-demo.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Cards from "@/components/demo/Cards";
+
+const CardDemo = () => {
+  return <Cards />;
+};
+
+export default CardDemo;
